9d2487c970056db9a63f78bf3b2e694faac74d5f
Andrefer-Santtos <1andre.santos.souza1@gmail.com>
2023-05-09 23:17:24 -0300

    Adicionando packages
    Foram adicionados diferentes pacotes que serão utilizados na construção da API seguindo o modelo MVC

--- src/main/java/write/your/magic/hp/HpApplication.java
+++ src/main/java/write/your/magic/hp/HpApplication.java
@@ -1,13 +1,18 @@
 package write.your.magic.hp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HpApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(HpApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(HpApplication.class, args);
+        log.info("HP-API STARTED!");
     }
 
 }
